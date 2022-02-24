b2bf09881e4639bca9d2615d01cd7833feafe705
Allen Smith <typhona@gmail.com>
2022-02-24 08:27:06 -0600

    Updated the way that classes are created for the links using the id argument when calling the function

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Repository for my helper functions for web development*.
 
 *Pre installed patented SHABLAKA!(Its just simple magic and wizardry.)
 
-The first function will be my navigation builder. Simple (probably over done) javascript navagation builder that requires 2 arrays, one is a list of  text for links, and the other for the actual list of file names/urls. 
-Working on adding drop down possibilities but for now this is just a plain Top level navigation generator. On the page it is require to have a ul or an ol with an ID.
+Simple (probably over done) javascript navagation builder that requires 2 arrays, one is a list of  text for links, and the other for the actual list of file names/urls. 
+Working on adding drop down possibilities but for now this is just a plain Top level navigation generator. 
+
+On the page it is require to have a ul or an ol with an ID. Latest update has used the 'id' argument for calling the function as a prefix for making classes in the resulting links.
 
 The footer.js is a super simple way to keep footer text uniform across an entire site. 
--- navBuilder.js
+++ navBuilder.js
@@ -1,27 +1,31 @@
 // Autofill navigation on all pages. use a UL with the class of "nav" for main navigation v1
 
 // Navigation Text and links listing
-let navText = [];
-let navLinks = [];
+let navText = ['House', 'Food', 'Work'];
+let navLinks = ['house.html', 'food.html', 'work.html'];
+
+
+
 
 
 function navBuilder(id, textList, linkList) {
 
   // Getting the element with the id
   navList = document.getElementById(id);
+  // let openTag = document.innerHTL
 
   // looping through the arrays and matching the text with the link
-  for (let i = 0; i < navText.length; i++) {
-    pageLinks = document.createElement('li')
+  for (let i = 0; i < textList.length; i++) {
 
-    // adds class name
-    pageLinks.className = 'nav_item';
-
+    listItem = document.createElement('li');
+    // adds classes
+    listItem.className = id + '_list_item';
+    linkClass = id + "_list_link";
     // put it all together
-    pageLinks.innerHTML += "<a href=\"" + linkList[i] + "\" class=\"nav_link\">" + textList[i] + "</a></li>";
-    navList.appendChild(this.pageLinks);
+    listItem.innerHTML += "<a href=\"" + linkList[i] + "\" class=\"" + linkClass + "\">" + textList[i] + "</a></li>";
+    navList.appendChild(listItem);
   }
 
 }
-// Call the function for now
+// Call the function here or include it in a script tag at the end of the html.
 navBuilder("nav", navText, navLinks);
